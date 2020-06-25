d3c2124eb079d6e2cff7fcdbf4f5fd30e26ff07d
SvenHarskamp <sven-harskamp@outlook.com>
2020-06-25 10:38:11 +0200

    -

--- ursim-3.6.0.30512/starturcontrol.sh
+++ ursim-3.6.0.30512/starturcontrol.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname $(readlink -f $0))
-echo yourpassword | sudo -S HOME=$SCRIPT_DIR $SCRIPT_DIR/URControl &>$SCRIPT_DIR/URControl.log &
+echo ros | sudo -S HOME=$SCRIPT_DIR $SCRIPT_DIR/URControl &>$SCRIPT_DIR/URControl.log &
